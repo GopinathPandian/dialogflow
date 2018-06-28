4318aa6fc391cd6fbfd198b310f4ea9b47ab6f60
GopinathPandian <40463691+GopinathPandian@users.noreply.github.com>
2018-06-28 18:04:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,10 @@ if($method == 'POST'){
 		case 'anything':
 			$speech = "Yes, you can type anything here.";
 			break;
-		
+		case 'what is the temperature of london':
+			$client = new GuzzleHttp\Client();
+			$res = $client->get('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=282def8ec7a8888ee244ce7c3b9880a0');
+			$speech = $res->getStatusCode();
 		default:
 			$speech = "Sorry, I didnt get that. Please ask me something else.";
 			break;
